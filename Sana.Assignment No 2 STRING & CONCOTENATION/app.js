5e52305571378362285ffd62704dca73227b61fd
sanakhan88 <Princessanasana786@gmail.com>
2025-09-18 18:45:40 +0500

    Add files via upload

---
+++ Sana.Assignment No 2 STRING & CONCOTENATION/app.js
@@ -0,0 +1,30 @@
+
+var street = prompt("Enter your street");
+var city = prompt("Enter your city");
+var country = prompt("Enter your country");
+var fullAddress = (street + ", " + city + ", " + country);
+alert("Your full address is:" + fullAddress);
+
+
+var firstName = prompt("Enter your first name");
+var favoriteNumber = prompt("Enter your favorite number");
+var username = (firstName + favoriteNumber);
+alert("Your new username is: " + username);
+
+
+var firstName = prompt("Enter your first name:");
+var lastName = prompt("Enter your last name:");
+var email = firstName + "." + lastName + "@example.com";
+alert("Your new email is: " + email);
+
+
+
+var color = prompt("What is your favorite color?");
+var animal = prompt("What is your favorite animal?");
+var food = prompt("What is your favorite food?");
+alert("Your favorite things are: " + color + " " + animal + " and " + food + "!");
+
+
+var quote = prompt("Enter your favorite quote:");
+var author = prompt("Who is the author?");
+alert('"' + quote + '" - ' + author);
