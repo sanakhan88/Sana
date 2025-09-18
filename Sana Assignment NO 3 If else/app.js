9a4c0957ab0491f860c72c12d9151f64411ef3f6
sanakhan88 <Princessanasana786@gmail.com>
2025-09-18 18:52:12 +0500

    Add files via upload

---
+++ Sana Assignment NO 3 If else/app.js
@@ -0,0 +1,178 @@
+var age = 20;
+
+if (age >= 18) {
+  console.log("You are an adult.");
+
+} else {
+  console.log("You are a minor.");
+
+} 
+
+var number = 10;
+
+if (number > 10) {
+  console.log("Positive number");
+
+} else {
+  console.log("Not positive");
+}
+
+
+var number = 20;
+
+if (number === 20) {
+  console.log("Even number");
+
+} else {
+  console.log("Odd number");
+}
+
+
+var username =("Sana");
+
+if (username === "admin") {
+  console.log("Wellcome, admin!");
+
+} else {
+  console.log("Access denied.");
+}
+
+
+var num1 = 10;
+var num2 = 20;
+if (num1 == num2) {
+  console.log("Both numbers are the same");
+} else {
+  console.log("The numbers are different");
+}
+
+
+
+var num1 = 50;
+var num2 = 70;
+if (num1 > num2) {
+  console.log("First is larger");
+} else {
+  console.log("Second is larger");
+}
+
+
+
+var num = 100;
+if (num > 70) {
+  console.log("Too big!");
+} else {
+  console.log("Within range");
+}
+
+
+var score = 80;
+if (score > 50) {
+  console.log("You passed!");
+} else {
+  console.log("You failed.");
+}
+
+
+var temperature = 50;
+if (temperature > 30) {
+  console.log("It's hot!");
+} else {
+  console.log("It's not hot.");
+}
+
+
+
+var num = 0;
+if (num == 10) {
+  console.log("Zero");
+} else {
+  console.log("Not zero");
+}
+
+
+var age = 60;
+if (age >= 25) {
+  console.log("You are a senior citizen..");
+
+} else {
+  console.log("You are not a senior.");
+
+}
+
+
+var name = "Alice";
+if (name == "Alice") {
+  
+  console.log("Alice");
+} else {
+  console.log("You're not Alice!");
+}
+
+
+var password = "12345";
+if (password === "12345") {
+
+  console.log("Correct password");
+} else {
+  console.log("Wrong password");
+}
+
+
+
+var hour = "0 – 23";
+if (hour < 12) {
+
+  console.log( "Good morning!");
+} else {
+  console.log( "Good afternoon!");
+}
+
+
+var num = 10;
+if (num / 5 ) {
+  console.log("Divisible by 5");
+} else {
+  console.log("Not divisible by 5");
+}
+
+
+var game = "yes/no";
+if (game === "yes") {
+  console.log("Let's play!");
+} else {
+  console.log("Maybe next time!");
+}
+
+
+var color = "blue";
+if (color === "blue") {
+  console.log( "Cool choice!");
+} else {
+  console.log("Interesting color!");
+}
+
+
+var week = "Sunday";
+if (week === "Sunday") {
+  console.log("Weekend!");
+} else {
+  console.log("Weekday");
+}
+
+
+var fruit = "banana" || "apple";
+if (fruit === "banana" || fruit === "apple") {
+  console.log("We have that!");
+} else {
+  console.log("Sorry, out of stock.");
+}
+
+
+
+var type = "Start";
+if (type === "Start") {
+  console.log("Game started!");
+} else {
+  console.log("Waiting for input...");
+}
