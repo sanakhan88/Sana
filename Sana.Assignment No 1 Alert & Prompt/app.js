fd94962f003fe86fe0e2f58bc7d6f9de13a9aa67
sanakhan88 <Princessanasana786@gmail.com>
2025-09-18 13:10:24 +0500

    Add files via upload

---
+++ Sana.Assignment No 1 Alert & Prompt/app.js
@@ -0,0 +1,24 @@
+
+var name = prompt("What is your name?");
+alert("Welcome, " + name + "!");
+
+
+
+var name = prompt("What is your name?");
+var age = prompt("what is your age?");
+console.log("name is " + age + " years old.");
+
+
+var color = prompt("What is your favorite color?");
+alert("Wow! " + color + " is a nice color!");
+
+
+var num1 = prompt("Enter the first number");
+var num2 = prompt("Enter the second number");
+console.log("The result of " + num1 + " + " + num2 + " is " + result + ".");
+
+
+var name = prompt("What is your name?");
+var hobby = prompt("What is your hobby?");
+var country = prompt("Which country are you from?");
+alert("Hi, I'm " + name + " from " + country + ", and I love " + hobby + "!");
